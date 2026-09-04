c3a8a8d98161cae91e461eebf384fe83f2777ffc
XiaoshiTwinkling <1730578@qq.com>
2026-09-04 23:34:59 +0800

    Initial commit

---
+++ src/client/java/com/xiaoshi/NatureWhisperClient.java
@@ -0,0 +1,10 @@
+package com.xiaoshi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NatureWhisperClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
